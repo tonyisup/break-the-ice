9026453ac4fdb4bf92984e093375a96f95d067be
tonyisup <antonioaccess@gmail.com>
2025-09-22 08:54:20 -0400

    feat: Add new gradient icon and enhance action buttons
    This commit introduces the `GradientArrowRightIcon` component for a visually appealing "Next" button in the `ActionButtons` component. The button now features a gradient and is positioned alongside the existing shuffle button, improving user experience and visual consistency during the shuffle process.

---
+++ src/components/ui/icons/GradientArrowRightIcon.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+interface GradientArrowRightIconProps {
+  size?: number;
+  gradient: (string | undefined)[];
+  className?: string;
+}
+
+export const GradientArrowRightIcon: React.FC<GradientArrowRightIconProps> = ({ size = 24, gradient, className }) => {
+  const gradientId = "icon-gradient";
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke={`url(#${gradientId})`}
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={className}
+    >
+      <defs>
+        <linearGradient id={gradientId} x1="0%" y1="0%" x2="100%" y2="100%">
+          <stop offset="0%" stopColor={gradient[0]} />
+          <stop offset="100%" stopColor={gradient[1]} />
+        </linearGradient>
+      </defs>
+      <path d="M12 16l4 -4l-4 -4" />
+      <path d="M8 12h8" />
+      <path d="M3 3m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z" />
+    </svg>
+  );
+};
