4f77c2170c48ca10c856a789562f9a28fe8fe2ff
Tony V <tony.villanueva@aptechnology.com>
2025-09-23 13:03:24 -0700

    disabling sign in until I'm ready for it

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,31 @@
-import { useStoreUserEffect } from "./hooks/useStoreUserEffect";
+// import { useStoreUserEffect } from "./hooks/useStoreUserEffect";
 import MainPage from "./pages/MainPage";
-import { SignIn } from "./SignIn";
-import { useTheme } from "./hooks/useTheme";
+// import { SignIn } from "./SignIn";
+// import { useTheme } from "./hooks/useTheme";
 import { ErrorBoundary } from "./components/ErrorBoundary";
 import CookieConsentBanner from "./components/CookieConsentBanner";
 
 export default function App() {
-  const { isAuthenticated, isLoading } = useStoreUserEffect();
+  // const { isAuthenticated, isLoading } = useStoreUserEffect();
 
-  const { effectiveTheme } = useTheme();
-  const gradientLight = ["#667EEA", "#A064DE"];
-  const gradient = ["#3B2554", "#262D54"];
-  if (isLoading) {
-    return (
-      <div
-        className="min-h-screen transition-colors overflow-hidden"
-        style={{
-        background: `linear-gradient(135deg, ${effectiveTheme === "dark" ? gradient[0] : gradientLight[0]}, ${effectiveTheme === "dark" ? gradient[1] : gradientLight[1]}, ${effectiveTheme === "dark" ? "#000" : "#fff"})`
-        }}
-      >
-      </div>
-    );
-  }
+  // const { effectiveTheme } = useTheme();
+  // const gradientLight = ["#667EEA", "#A064DE"];
+  // const gradient = ["#3B2554", "#262D54"];
+  // if (isLoading) {
+  //   return (
+  //     <div
+  //       className="min-h-screen transition-colors overflow-hidden"
+  //       style={{
+  //       background: `linear-gradient(135deg, ${effectiveTheme === "dark" ? gradient[0] : gradientLight[0]}, ${effectiveTheme === "dark" ? gradient[1] : gradientLight[1]}, ${effectiveTheme === "dark" ? "#000" : "#fff"})`
+  //       }}
+  //     >
+  //     </div>
+  //   );
+  // }
 
-  if (!isAuthenticated) {
-    return <SignIn />;
-  }
+  // if (!isAuthenticated) {
+  //   return <SignIn />;
+  // }
 
   return (
     <ErrorBoundary>
