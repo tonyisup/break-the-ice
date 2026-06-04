6083b14a6457fa52414696a441b4ecb17688c27d
tony mac <antonioaccess@gmail.com>
2026-06-04 12:11:44 -0700

    feat: extend Tailwind CSS configuration to include Clerk theme scripts
    Updated the Tailwind CSS configuration to include JavaScript files from the Clerk themes package, enhancing the styling capabilities for Clerk components within the application. This addition supports improved theming and appearance management in line with recent updates to the ClerkProvider.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/@clerk/themes/dist/**/*.js",
+  ],
   theme: {
     container: {
       center: true,
